75026232d44ec0b909c7b5d324db7b27bfed4e07
pluu2 <paulvss2000@gmail.com>
2021-04-25 11:55:59 -0400

    attention.py, convolutions.py

--- attention.py
+++ attention.py
@@ -1,5 +1,5 @@
 import jax.numpy as jnp
 def softmaxT (logits,temperature=1): #default temperature is 1
-  return jnp.exp(logits/temperature) / np.sum(np.exp(logits/temperature), axis=0)
+  return jnp.exp(logits/temperature) / jnp.sum(jnp.exp(logits/temperature), axis=0)
 def attention(q,k,temp=1): 
   return softmaxT(jnp.dot(q,k.T),temperature=temp)
